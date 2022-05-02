25257a711b94583f97ad3b53a3239ccd7d0853ba
bezao244 <bernardoafonso9734@gmail.com>
2022-05-02 12:21:41 -0300

    Fds

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,28 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  user ={
-    userName: 'bezao',
-    pass: 'bezao123'
-  }
+  crudForm: FormGroup;
 
   constructor(
     private authService: AuthService,
-    private router:Router
+    private router:Router,
+    private formBuilder: FormBuilder,
     ) { }
 
   ngOnInit(): void {
+    this.crudForm = this.formBuilder.group({
+      userName: [null],
+      pass: [null]
+    });
   }
   login(){
-    this.authService.singin(this.user).subscribe( (res:any) => {
+    this.crudForm.patchValue({
+      userName: this.crudForm.value.userName,
+      pass: this.crudForm.value.pass
+    });
+
+    this.authService.singin(this.crudForm.value.userName).subscribe( (res:any) => {
       localStorage.setItem('token', res.token);
       this.router.navigate(['private']);
     });
